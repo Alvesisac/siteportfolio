85503300a0ffea99dc591e637ab25325f5152eda
Alvesisac <141432991+Alvesisac@users.noreply.github.com>
2023-11-21 11:33:02 -0300

    Add files via upload

---
+++ profile/js/script.js
@@ -0,0 +1,10 @@
+function enviarmensagem() {
+ var nome= document.getElementById("idNone");
+
+ if  (nome.value != "") {
+
+    alert("Obrigado, sr(a)" + nome.value + ", sua mensagem foi enviada com sucesso!"); 
+  }
+
+
+}
